30364cff88f16357712b8ece67b2486cbdde62ba
YURA <shlyapin.y.a@gmail.com>
2021-04-18 18:40:00 +0400

    calculator

--- src/main/java/org/example/controller/Controller1.java
+++ src/main/java/org/example/controller/Controller1.java
@@ -1,21 +1,48 @@
 package org.example.controller;
 
+import org.example.logick.Colcul;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class Controller1 {
     @GetMapping("/")
     public String home(){
-        return "index.html";
+        return "index";
     }
     @GetMapping("/about")
     public String about(){
-        return "about.html";
+        return "about";
     }
     @GetMapping("/java")
     public String java(){
-        return "java.html";
+        return "java";
+    }
+    @GetMapping("/people")
+    public String people(Model model){
+
+        model.addAttribute("a","все это стоит "+200+" рублей");
+        return "people";
+    }
+    @GetMapping("/get-count")
+    public String count(Model model,
+                        @RequestParam(value = "a",required = false)Integer a){
+        String coun = "кола стоит "+a+" рублей";
+        model.addAttribute("a",coun);
+        return "count";
+    }
+    @GetMapping("/colcul")
+    public String colcul(Model model,
+                        @RequestParam(value = "a",required = false)Integer a,
+                         @RequestParam(value = "b",required = false)Integer b,
+                         @RequestParam(value = "c",required = false)Character c){
+        Colcul colcul = new Colcul(a,b,c);
+        String answer = a+""+c.toString()+""+b+"="+colcul.answer();
+
+        model.addAttribute("answer",answer);
+        return "colcul";
     }
 }
--- src/main/java/org/example/logick/Colcul.java
+++ src/main/java/org/example/logick/Colcul.java
@@ -1,2 +1,28 @@
-package org.example.logick;public class Collcul {
+package org.example.logick;
+
+public class Colcul {
+    Integer a;
+    Integer b;
+    Character c;
+
+    public Colcul(Integer a, Integer b, Character c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    public Integer answer(){
+        Integer answer=0;
+        if (c=='+'){
+            answer=a+b;
+        }else if (c=='-'){
+            answer=a-b;
+        }else if (c=='/'){
+            answer=a/b;
+        }else if (c=='*'){
+            answer=a*b;
+        }else if (c=='%'){
+            answer=a%b;
+        }
+        return answer;
+    }
 }
