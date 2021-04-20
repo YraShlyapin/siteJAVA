df4095a7abe049a69978b3362ddf1e6b198891c4
YURA <shlyapin.y.a@gmail.com>
2021-04-20 18:51:07 +0400

    Pizza

--- src/main/java/org/example/controller/Controller1.java
+++ src/main/java/org/example/controller/Controller1.java
@@ -4,7 +4,11 @@ import org.example.logick.Colcul;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.lang.reflect.Member;
 
 
 @Controller
@@ -36,10 +40,16 @@ public class Controller1 {
     }
     @GetMapping("/colcul")
     public String colcul(Model model,
-                        @RequestParam(value = "a",required = false)Integer a,
+                         @RequestParam(value = "a",required = false)Integer a,
                          @RequestParam(value = "b",required = false)Integer b,
                          @RequestParam(value = "c",required = false)Character c){
         Colcul colcul = new Colcul(a,b,c);
+        if (c=='p'){
+            c='+';
+        }
+        if (c=='o'){
+            c='%';
+        }
         String answer = a+""+c.toString()+""+b+"="+colcul.answer();
 
         model.addAttribute("answer",answer);
